9b09b3958bd7dc18ff0103b0752156e7eff2a492
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2025-02-05 17:48:12 +0000

    Adjust formatting

--- src/FormParser.jsx
+++ src/FormParser.jsx
@@ -5,7 +5,7 @@ import { TextBox, DropDown, MultipleAnswerQuestions } from './FormGenerator'
 import './App.css'
 import { GetLanguages } from './LanguagesDropDown'
 import {questions} from "./SampleForm.json"
-import { TextInput, Checkbox, Radio, Stack, Group } from '@mantine/core'
+import { TextInput, Checkbox, Radio, Stack, Group, Container } from '@mantine/core'
 import { useForm } from '@mantine/form'
 
 export function FormParser() {
@@ -22,47 +22,50 @@ export function FormParser() {
     const [submittedValues, setSubmittedValues] = useState([]);
 
     return (
-        <Stack 
-            justify='flex-start'
-            align='flex-start'
-            gap="xl">
-        {
-        items.map((item, index) => {
-                if(item.type === "textbox"){
-                    return (
-                        // <TextBox key={item.id || index} question={item.question}/>
+        <Container size="xs" pt={30}>
+            <Stack 
+                justify='flex-start'
+                align='stretch'
+                gap="xl"
+                >
+                {
+                items.map((item, index) => {
+                        if(item.type === "textbox"){
+                            return (
+                                // <TextBox key={item.id || index} question={item.question}/>
 
-                        <TextInput key={item.id || index} label={item.question} />
-                    )
-                }
-                if(item.type === "checkbox"){
-                    return (
-                        // <MultipleAnswerQuestions key={item.id || index}  question={item.question} answers={item.answers} type="checkbox"/>
+                                <TextInput key={item.id || index} label={item.question} />
+                            )
+                        }
+                        if(item.type === "checkbox"){
+                            return (
+                                // <MultipleAnswerQuestions key={item.id || index}  question={item.question} answers={item.answers} type="checkbox"/>
 
-                        <Checkbox.Group key={item.id || index} label={item.question} >
-                            <Stack gap="xs">
-                            {item.answers.map( (answer, index) => (
-                                <Checkbox key={item.id || index} value={answer} label={answer}/>
-                            ))}
-                            </Stack>
-                        </Checkbox.Group>
-                    )
-                }
-                if(item.type === "radio"){
-                    return (
-                        // <MultipleAnswerQuestions key={item.id || index}  question={item.question} answers={item.answers} type="radio"/>
+                                <Checkbox.Group key={item.id || index} label={item.question} >
+                                    <Stack gap="xs">
+                                    {item.answers.map( (answer, index) => (
+                                        <Checkbox key={item.id || index} value={answer} label={answer}/>
+                                    ))}
+                                    </Stack>
+                                </Checkbox.Group>
+                            )
+                        }
+                        if(item.type === "radio"){
+                            return (
+                                // <MultipleAnswerQuestions key={item.id || index}  question={item.question} answers={item.answers} type="radio"/>
 
-                        <Radio.Group key={item.id || index} label={item.question} >
-                            <Stack gap="xs">
-                            {item.answers.map( (answer, index) => (
-                                <Radio key={item.id || index} value={answer} label={answer}/>
-                            ))}
-                            </Stack>
-                        </Radio.Group>
-                    )
+                                <Radio.Group key={item.id || index} label={item.question} >
+                                    <Stack gap="xs">
+                                    {item.answers.map( (answer, index) => (
+                                        <Radio key={item.id || index} value={answer} label={answer}/>
+                                    ))}
+                                    </Stack>
+                                </Radio.Group>
+                            )
+                        }
+                })
                 }
-        })
-        }
-        </Stack>
+            </Stack>
+        </Container>
     )
 }
