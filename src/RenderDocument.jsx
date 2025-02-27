6193c93aa423a65c5db517d9cd2fd698ad785442
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2025-02-27 15:28:15 +0000

    Implement translation of document text in RenderDocument.jsx

--- src/RenderDocument.jsx
+++ src/RenderDocument.jsx
@@ -1,8 +1,26 @@
 import { Text } from "@mantine/core"
+import { Translator } from "./Translator"
+import { useEffect, useState } from "react"
 
 const RenderDocument = ({ text, targetLanguage="es" }) => {
+    const [translatedText, setTranslatedText] = useState([]);
 
-    return <Text>{text}</Text>
+    useEffect(() => {
+        const translateText = async () => {
+            try {
+                const translatedText = await Translator(targetLanguage, text) ?? "err"
+                setTranslatedText(translatedText)
+            } catch (error) {
+                console.error("Error loading or translating text:", error)
+            }
+        }
+
+        // Don't run Translator if user has already selected english
+        if (targetLanguage === "en") setTranslatedText(text)
+        translateText();
+    }, [targetLanguage])
+    
+    return <Text>{translatedText}</Text>
 }
 
 export default RenderDocument
